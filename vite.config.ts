7d0e2c3d94c536141a8ab147896f67ed454dd502
Sergei Cherniaev <sergei@cherniaev.com>
2024-09-18 11:23:10 +0400

    feat(pwa) add web manifest to make it installable as an app on devices that support PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: null, // Disable service worker registration
+      manifest: {
+        name: "Number Streak – listen and learn numbers in foreign languages",
+        short_name: "Listen Numbers",
+        description: "Practice listening skills with Number Streak!",
+        id: "NumStr",
+        start_url: "/",
+        theme_color: "#1f69ff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "regular_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "regular_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable_icon_x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
