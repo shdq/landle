eab4ab4f9aae9463aa4d6b95447e0d5389dde48c
Sergei Cherniaev <sergei@cherniaev.com>
2024-09-28 20:53:17 +0400

    feat(languages): add support of other languages (#9)
    * feat(languages): add support of english, german, french, italian, and russian languages
    
    * chore: update components library to bring select component
    
    * feat(languages): add select component from spartak-ui

--- src/App.tsx
+++ src/App.tsx
@@ -14,41 +14,83 @@ import {
   CardHeader,
   CardBody,
   CardFooter,
+  Select,
 } from "spartak-ui";
 import "./App.css";
 import { getNextUniqueRandomNumber, generateOptions } from "./utils/functions";
 
+const DEFAULT_LANG = "en";
+enum LANGUAGES {
+  de = "🇩🇪 German",
+  es = "🇪🇸 Spanish",
+  en = "🇬🇧 English",
+  fr = "🇫🇷 French",
+  it = "🇮🇹 Italian",
+  ru = "🇷🇺 Russian",
+}
+
 function App() {
-  const [score, setScore] = useState(() => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      // Load record from local storage or default to 0
-      return parseInt(localStorage.getItem("score-es") || "0", 10);
-    } else {
-      return 0;
-    }
-  });
-  const [record, setRecord] = useState(() => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      // Load record from local storage or default to 0
-      return parseInt(localStorage.getItem("record-es") || "0", 10);
-    } else {
-      return 0;
-    }
-  });
+  const [currLang, setCurrLang] =
+    useState<keyof typeof LANGUAGES>(DEFAULT_LANG);
+  const [score, setScore] = useState(0);
+  const [record, setRecord] = useState(0);
   const [number, setNumber] = useState(getNextUniqueRandomNumber());
   const [isStarted, setIsStarted] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false); // is app component loaded
 
+  // load default settings
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
-      localStorage.setItem("score-es", score.toString());
+      // Load last used language from the local storage or default to DEFAULT_LANG
+      const lastOrDefaultLang =
+        (localStorage.getItem(`last-used-lang`) as keyof typeof LANGUAGES) ||
+        DEFAULT_LANG;
+      setCurrLang(lastOrDefaultLang);
+
+      // Load last score for the last used lang from local storage or default to 0
+      setScore(
+        parseInt(localStorage.getItem(`score-${lastOrDefaultLang}`) || "0", 10)
+      );
+      // Load last record for the last used lang from local storage or default to 0
+      setRecord(
+        parseInt(localStorage.getItem(`record-${lastOrDefaultLang}`) || "0", 10)
+      );
+    }
+    setIsLoaded(true); // the component is loaded and has access to localStorage
+  }, []);
+
+  useEffect(() => {
+    if (isLoaded) {
+      localStorage.setItem(`score-${currLang}`, score.toString());
     }
   }, [score]);
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.localStorage) {
-      localStorage.setItem("record-es", record.toString());
+    if (isLoaded) {
+      localStorage.setItem(`record-${currLang}`, record.toString());
     }
   }, [record]);
+
+  useEffect(() => {
+    if (isLoaded) {
+      const langScore = parseInt(
+        localStorage.getItem(`score-${currLang}`) || "0",
+        10
+      );
+      setScore(langScore);
+      const langRecord = parseInt(
+        localStorage.getItem(`record-${currLang}`) || "0",
+        10
+      );
+      setRecord(langRecord);
+
+      // update last used language when user changes the language
+      localStorage.setItem(`last-used-lang`, currLang);
+
+      // when user changes the language regenerate number against cheating
+      setNumber((prev) => getNextUniqueRandomNumber(prev));
+    }
+  }, [currLang]);
 
   const speakNumber = (number: number, speed: number = 1) => {
     // Check if window and speechSynthesis are available
@@ -59,7 +101,7 @@ function App() {
       const utterance = new SpeechSynthesisUtterance(number.toString());
 
       // Set language and playback speed
-      utterance.lang = "es-ES"; // Spanish language
+      utterance.lang = `${currLang.toLowerCase()}-${currLang}`;
       utterance.rate = speed; // Speed (1 is default)
 
       // Speak the number
@@ -74,6 +116,16 @@ function App() {
       speakNumber(number);
     }
   }, [number]);
+
+  const selectOptions = Object.entries(LANGUAGES).map(
+    ([langCode, langName]) => {
+      return { value: langCode, label: langName, key: langCode };
+    }
+  );
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrLang(e.target.value as keyof typeof LANGUAGES);
+  };
 
   const optionsButtons = generateOptions(number).map((num) => {
     return (
@@ -110,9 +162,30 @@ function App() {
           <Heading as="h1" size="xl">
             Numstr{" "}
             <Text size="lg" secondary>
-              What's your longest streak?
+              Listen and learn numbers
             </Text>
           </Heading>
+          <br />
+          <Card
+            css={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text size="lg">I'm studying</Text>
+            <Select
+              css={{ minWidth: "140px" }}
+              size="md"
+              name="languages"
+              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                handleSelectChange(e);
+              }}
+              value={currLang}
+              options={selectOptions}
+            />
+          </Card>
           <br />
           <Text size="lg">
             Points{" "}
