a509db89360f01ce869c755b5ee2c7fff61423bd
Sergei Cherniaev <sergei@cherniaev.com>
2024-10-20 14:58:16 +0400

    feat: add static files, update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -17,14 +17,32 @@ export default defineConfig({
         description: "Practice listening skills with Number Streak!",
         id: "NumStr",
         start_url: "/",
-        theme_color: "#1f69ff",
+        theme_color: "#0A5CFF",
         background_color: "#ffffff",
         display: "standalone",
         orientation: "portrait",
         icons: [
           {
-            src: "regular_icon_x512.png",
+            src: "landle_icon_x1024.png",
+            sizes: "1024x1024",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "landle_icon_x512.png",
             sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "landle_icon_x384.png",
+            sizes: "384x384",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "landle_icon_x192.png",
+            sizes: "192x192",
             type: "image/png",
             purpose: "any",
           },
@@ -33,12 +51,6 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
-          },
-          {
-            src: "regular_icon_x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
           },
           {
             src: "maskable_icon_x192.png",
